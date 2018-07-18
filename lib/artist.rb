5660df042385a91aa3b30fe8cab37c305f7a51ee
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-18 17:52:15 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect { |artist| artist.name = name } || Artist.new(name)
+  end
+
+  def add_song(song)
+    @songs.push(song) unless @songs.include?(song)
+    song.artist = self unless song.artist == self
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def print_songs
+      @songs.each { |song| puts song.name }
+  end
+
+end
