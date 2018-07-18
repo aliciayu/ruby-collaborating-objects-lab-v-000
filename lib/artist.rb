545c062373d97d7e3f6563edd6e73a536e93b2f5
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-18 18:14:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,38 @@
-class Artist
-
-  attr_accessor :name
-  attr_reader :songs
-
+lass Artist
   @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
     @songs = []
-    self.class.all << self
   end
 
   def self.all
-    @@all
-  end
-
-  def self.find_or_create_by_name(name)
-    self.all.detect { |artist| artist.name = name } || Artist.new(name)
+    @@all    
   end
 
   def add_song(song)
-    @songs.push(song) unless @songs.include?(song)
-    song.artist = self unless song.artist == self
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
   end
 
   def save
     @@all << self
-    self
   end
 
   def print_songs
-      @songs.each { |song| puts song.name }
+    songs.each {|song| puts song.name}
   end
-
 end
