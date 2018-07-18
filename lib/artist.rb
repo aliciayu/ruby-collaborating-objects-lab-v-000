498609749fbe021485ba66252385554ac3474f11
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-18 18:08:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,17 +15,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.crate_by_name(name)
-  end
-
-  def self.find_by_name(name)
-    @@all.detect do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create_by_name(name)
-    self.new(name).save
+    self.all.detect { |artist| artist.name = name } || Artist.new(name)
   end
 
   def add_song(song)
@@ -34,12 +24,12 @@ class Artist
   end
 
   def save
-    @@all << self
+    @@all << self)
     self
   end
 
   def print_songs
-      @songs.each {|s| puts s.name}
+      @songs.each { |song| puts song.name }
   end
 
 end
